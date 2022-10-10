97d495d30d553c2387705bb66ccce1f2af101cb7
nodir <nodirbarotov0707@gmail.com>
2022-10-10 21:02:26 +0500

    'nothing'

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
